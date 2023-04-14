43da9f254f1f30ff6f68db42c7ceb8071bbd07b7
TechChefMM <mmendoza@mail.com>
2023-04-14 10:44:58 +0000

    update udacity training project file.  202304140644.  MM

--- starter_files/make_predict_azure_app.sh
+++ starter_files/make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://mywebapp20230410.azurewebsites.net:$PORT/predict 
+     -X POST https://mywebapp20230414.azurewebsites.net:$PORT/predict 
      # TODO: Replace the <yourappname> with your application name 
